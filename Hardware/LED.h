87c6e8ca98ca9031673df6f0ca6a900a45ce4536
luoyin673-cmd <luoyin673@gmail.com>
2026-06-17 17:11:44 +0800

    first commit

---
+++ Hardware/LED.h
@@ -0,0 +1,12 @@
+#ifndef _LED_H
+#define _LED_H
+
+void LED_Init(void);
+void LED0_ON(void);
+void LED0_OFF(void);
+void LED1_ON(void);
+void LED1_OFF(void);
+void LED0_Turn();
+void LED1_Turn();
+
+#endif
